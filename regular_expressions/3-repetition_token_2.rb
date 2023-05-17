42c08e7aea6201f2d307ad1c863991b8105f0a6e
FoibeU <f.uwizeyima@gmail.com>
2023-05-17 09:35:02 -0700

    Matching

--- regular_expressions/3-repetition_token_2.rb
+++ regular_expressions/3-repetition_token_2.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 
 if ARGV.empty?
-  puts "Enter argument"
+  puts "Enter string to test"
   exit 
 end
-
 input = ARGV[0]
-pattern= /h[b]tn/
+pattern = /hb[t]{1,4}n/
 matches = input.scan(pattern)
 if matches
   puts "#{matches.join()}"
 else
   puts ""
+  exit
 end
