31adf369948d1de7bccf7fb2ae69ef5d104bfc8c
FoibeU <f.uwizeyima@gmail.com>
2023-05-18 08:18:02 -0700

    text me

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,21 +1,15 @@
 #!/usr/bin/env ruby
-def extract_transaction_info(transaction)
-  sender = transaction.match(/^From:\s*(.+)/)&.captures&.first
-  receiver = transaction.match(/^To:\s*(.+)/)&.captures&.first
-  flags = transaction.scan(/\[(.+?)\]/).flatten.join(", ")
 
-  [sender, receiver, flags]
+if ARGV.empty?
+  puts "Enter digit to test"
+  exit
 end
-transactions = [
-  "From: John Kalisa [RWA]\nTo: Alice Mwanzia[UG]\n[FLAG1][FLAG2]",
-  "From: Blenda zagabe [DRC]\nTo: Caroline [KN]\n[FLAG3]",
-  "From: David\nTo: Emily\n",
-  "To: Sarah\n[BU]"
-]
-transactions.each do |transaction|
-  sender, receiver, flags = extract_transaction_info(transaction)
-  puts "Sender: #{sender}"
-  puts "Receiver: #{receiver}"
-  puts "Flags: #{flags}"
-  puts "\n"
+input = ARGV[0]
+pattern = /\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/
+matches = input.scan(pattern)
+if matches
+  puts "#{matches.join()}"
+else
+  puts ""
+  exit
 end
